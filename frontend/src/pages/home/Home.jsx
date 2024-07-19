9f651a962026fa5feba13dad076b8e4046566596
Tejas Patne <tejaspatne18@gmail.com>
2024-07-19 12:00:38 +0530

    changing message window position for mobile view

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import Sidebar from "../../components/sidebar/Sidebar.jsx";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-sky-950 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30'>
+		<div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-sky-950 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30'>
 			<Sidebar />
 			<MessageContainer />
 		</div>
