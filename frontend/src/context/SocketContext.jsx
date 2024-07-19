fed41a513c874a0e563f1bfbd925195e83fbcc75
Tejas Patne <tejaspatne18@gmail.com>
2024-07-19 11:37:44 +0530

    update socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("https://mern-chat.onrender.com", {
+            const socket = io("https://mern-chat-xgey.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
